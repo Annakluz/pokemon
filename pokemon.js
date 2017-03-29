a43ddaf73ef020e790711e40c1daee98ceb9863d
Annakluz <anna.luz.duran@gmail.com>
2017-03-28 23:06:24 -0600

    subir pokemons en lista

--- pokemon.js
+++ pokemon.js
@@ -11,11 +11,37 @@ function Pokemon(nombre, color, puntosAtaque){
     }
 }
 
-function crearPokemon(){
+var pokemons[];
+
+function crearPokemon() {
     var nombrePokemon = document.getElementById("nombrePokemon");
     var colorPokemon = document.getElementById("colorPokemon");
     var puntosAtaque = document.getElementById("puntosAtaque");
     var pokemon = new Pokemon(nombrePokemon.value,
                               colorPokemon.value,
-                              puntosAtaque.value,)
+                              parseInt(puntosAtaque.value,))
+    pokemons.push(pokemon)
+    mostrarPokemons();
+}
+console.log(pokemon);
+
+function mostrarPokemons(){
+    var listaPokemons = document.getElementById("listaPokemons");
+    var lista = document.createElement("ul");
+    
+    pokemons.forEach(function(pokemon){
+        var elemento = document.createElement("li");
+        
+        elemento.innerHTML=pokemon.nombre + pokemon.color + pokemon.puntosAtaque;
+        lista.appendChild(elemento);
+    })
+    listaPokemons.appendChild(lista);
+ /*   var strFinal="";
+    
+    pokemons.forEach(function(pokemon){
+                     strFinal +=pokemon.nombre
+                     strFinal +=pokemon.color
+                     strFinal +=pokemon.puntosAtaque
+                     })
+    listaPokemons.innerHTML=strFinal;*/
 }
